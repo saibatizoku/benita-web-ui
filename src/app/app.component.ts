aef0d66222af46acfa8afd8a9a1e41ecc16cfa7e
Joaquin Rosales <globojorro@gmail.com>
2017-09-09 17:23:46 -0500

    appropriate example from angular.io (thanks, guys), and tune some benita in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export class Sensor {
+  id: number;
+  name: string;
+}
+
+const SENSORS: Sensor[] = [
+  { id: 1, name: 'Temperature' },
+  { id: 2, name: 'Conductivity' },
+  { id: 3, name: 'pH' },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'benita';
+  sensors = SENSORS;
+  selectedSensor: Sensor;
+ 
+  onSelect(sensor: Sensor): void {
+    this.selectedSensor = sensor;
+  }
 }
